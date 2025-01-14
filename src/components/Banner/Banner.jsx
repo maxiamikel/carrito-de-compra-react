1c961690eb06fce9c33cc4a9f18519842d7b2222
maxiamikel19 <maxloversist19@gmail.com>
2025-01-14 09:27:16 -0300

    Navbar e Banner impl

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,16 @@
 import "./Banner.css";
 
 const Banner = () => {
-  return <div className="banner">Banner</div>;
+  return (
+    <div className="banner">
+      <div className="banner-container">
+        <h1 className="banner-title">Tu espacio de excelencia</h1>
+        <p className="banner-text">
+          Aqui, encontras hasta lo que no necesita y de buena calidad{" "}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import { IoIosCart } from "react-icons/io";
 import "./Navbar.css";
 const Navbar = () => {
   return (
     <div className="nav-container">
       <nav className="navbar">
-        <h1 className="nav-logo">Shop.</h1>
-        <h2 className="nav-cart">Carrito</h2>
+        <h1 className="nav-logo">
+          <a href="#">Shop.</a>
+        </h1>
+        <IoIosCart className="nav-cart" />
       </nav>
     </div>
   );
